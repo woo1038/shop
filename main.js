8bc1c7a53b49e4c23a30eac453af3dcb3dd031c2
hyuk <woo1038@hanmail.net>
2021-07-22 13:03:43 +0900

    banner layout 추가

--- main.js
+++ main.js
@@ -48,6 +48,104 @@ window.onload = function() {
       }
     })
   }
+
+
+  /* banner items array layout */
+  let banner_arr = []
+  let best_items = document.querySelectorAll(".best-item");
+  let banner_item = document.querySelector(".banner-item");
+  
+  for(let i=0; i<best_items.length; i++) {
+    let cnt = 0;  //상품별로 1,2,3 순서대로 배열 하기 위한 변수
+
+    for(let k=0; k<best_items.length; k++) {
+      cnt++;
+      if (banner_arr.length >= 9) {
+        break;
+      }
+      banner_arr.splice(k+cnt, 0, best_items[k].children[i]);
+    }
+  }
+
+  Array.from(banner_arr).map(item => {
+    /* item */
+    let item_class = item.classList[0];
+    let item_onclick = item.getAttribute("onclick");
+    let item_name = item.getAttribute("name");
+    let item_price = item.getAttribute("price");
+    let item_summary = item.getAttribute("summary");
+    let item_info = item.getAttribute("info");
+    let item_color = item.getAttribute("color");
+    let item_size = item.getAttribute("size");
+    let item_img = item.children[0].children[1].src;
+    
+    /* create */
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    let div_item = document.createElement("div");
+
+    /* setAttr */
+    li.className = item_class;
+    li.setAttribute("onclick", item_onclick);
+    li.setAttribute("name", item_name);
+    li.setAttribute("price", item_price);
+    li.setAttribute("summary", item_summary);
+    li.setAttribute("info", item_info);
+    li.setAttribute("color", item_color);
+    li.setAttribute("size", item_size);
+
+    /* latout */
+    for(let i=0; i<2; i++) {
+      let div = document.createElement("div");
+
+      switch (i) {
+        case 0:
+          div.className ="banner-img";
+            li.appendChild(div);
+            img.src = item_img;
+            div.appendChild(img);
+          break;
+        case 1:
+            for(let k=0; k<=4; k++) {
+              let label = document.createElement("label");
+
+              switch (k) {
+                case 0:
+                    label.innerHTML = item_name;
+                  break;
+                  
+                case 1:
+                    label.innerHTML = item_price;
+                  break;
+                  
+                case 2:
+                    label.innerHTML = item_summary;
+                  break;
+                  
+                case 3:
+                    label.innerHTML = item_info;
+                  break;
+                  
+                case 4:
+                    div_item.className = "color-box"
+                    div.appendChild(div_item); 
+    
+                    for(let j=0; j<item_color.split(" ").length; j++) {
+                      let span = document.createElement("span");
+                      span.className = item_color.split(" ")[j];
+                      div_item.appendChild(span);
+                    }
+                  break;
+              }
+              div.className = "banner-info"
+              div.appendChild(label);
+            }
+            li.appendChild(div);
+          break;
+      }
+    }
+    banner_item.appendChild(li);
+  });
 }
 
 
@@ -86,12 +184,4 @@ function get_cookie(cookie_name) {
       return unescape(y); // unescape로 디코딩 후 값 리턴
     }
   }
-}
-
-function add_cookie() {
-
-}
-
-function remove_cookie() {
-
 }
